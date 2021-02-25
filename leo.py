eda059be7c2032e8e35cca7e753c6d00a2d1397a
Antoine <antoine.sauvage@melix.net>
2021-02-25 20:21:40 +0100

    loop

--- leo.py
+++ leo.py
@@ -1,65 +1,66 @@
 from file_management import *
-import numpy as np
 
 
-f = read_file('input/c.txt')
-# f = read_file('input/b.txt')
+files_list = ["a", "b", "c", "d", "e", "f"]
 
-duration, nb_intersections, nb_streets, nb_cars, bonus_point, S, list_streets, list_cars = f
+for letter in files_list:
+    f = read_file('input/'+ letter +'.txt')
+    # f = read_file('input/b.txt')
 
-    # list_streets.append([begin_intersection, end_intersection, index_street, length])
-# print(list_cars)
-# print(list_streets)
-print(f)
+    duration, nb_intersections, nb_streets, nb_cars, bonus_point, S, list_streets, list_cars = f
 
-# nb visit de chaque street
-nb_visit = [0]*nb_streets
-for car in list_cars:
-    for street in car:
-        nb_visit[street] += 1
+        # list_streets.append([begin_intersection, end_intersection, index_street, length])
+    # print(list_cars)
+    # print(list_streets)
+    #print(f)
 
-inter = []
-# initialisation
-for i in range(nb_intersections):
-    inter.append(dict())
+    # nb visit de chaque street
+    nb_visit = [0]*nb_streets
+    for car in list_cars:
+        for street in car:
+            nb_visit[street] += 1
 
-for i in range(nb_streets):
-    inte = list_streets[i][1]
-    print(inte,S[i])
-    inter[inte][S[i]] = nb_visit[i]
+    inter = []
+    # initialisation
+    for i in range(nb_intersections):
+        inter.append(dict())
 
-# pour chaque inter, quelles street y arrivent et combien de fois elles sont visitees
+    for i in range(nb_streets):
+        inte = list_streets[i][1]
+        #print(inte,S[i])
+        inter[inte][S[i]] = nb_visit[i]
 
-print(inter)
+    # pour chaque inter, quelles street y arrivent et combien de fois elles sont visitees
 
-out = dict()
-# initialisation
-for i in range(nb_intersections):
-    out[i] =dict()
+    #print(inter)
 
-#boucle sur les inters qui calcule le ratio de chaque rue
-for i in range(nb_intersections):
-    rues = list(inter[i].keys())
-    times =[inter[i][r] for r in rues]
-    tot = sum(times)
-    print(inter[i])
-    print(times)
-    # min_visit
-    for street in rues:
-        if inter[i][street] ==0 and (tot==0):
-            t=1
-            out[i][street] = t
-            
-        else:
-            if inter[i][street] !=0:
-                t = int(4*inter[i][street] / (tot +0.1)) +1
+    out = dict()
+    # initialisation
+    for i in range(nb_intersections):
+        out[i] =dict()
+
+    #boucle sur les inters qui calcule le ratio de chaque rue
+    for i in range(nb_intersections):
+        rues = list(inter[i].keys())
+        times =[inter[i][r] for r in rues]
+        tot = sum(times)
+
+        # min_visit
+        for street in rues:
+            if inter[i][street] ==0 and (tot==0):
+                t=1
                 out[i][street] = t
-        
+                
+            else:
+                if inter[i][street] !=0:
+                    t = int(4*inter[i][street] / (tot +0.1)) +1
+                    out[i][street] = t
+            
 
-        print(street,t)
+  
 
 
-write_output(out,'c_frac.txt')
+    write_output(out,letter +'_frac.txt')
 
 
 
